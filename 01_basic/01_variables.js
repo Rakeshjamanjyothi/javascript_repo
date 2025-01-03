7fe3fbc7cc4804a71f4b2279ba49b4d5dedff5d9
Rakeshjamanjyothi <96175305+Rakeshjamanjyothi@users.noreply.github.com>
2025-01-03 16:06:23 +0530

    Add files via upload

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+console.log("Hi Programmer");
+
+const accountId = 121212;
+let accountName = "rakesh"; //scoped
+var accountEmail = "rakesh@gmail.com";
+accountName = "hello program";
+console.log(accountName);
+
+let accountState; //undifine
+
+console.table([accountId, accountEmail, accountName, accountState]);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all the js code as New Version
+//alert("helllo");//error message
+
+//here we are using node js , alert works in the browser
+
+//String :-
+
+let name = "helllo"; //string
+let age = 27; //number
+let islogin = false; //boolean
+let hi = null; //empty value
+let hello; //undefined ->   value not defined
+//symbol  -> defines Uniqueness
+
+console.log(typeof null); //Object
+console.log(typeof undefined); //undefined
